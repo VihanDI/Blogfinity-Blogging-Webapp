c6f8e5581b34126a4dc02cd74af5a8c32b86c30f
Inupa Vihan <inupavihan9@gmail.com>
2023-10-14 16:23:18 +0530

    Configure database connection
    Add .env for configure data
    Add validation for .env
    Refactor code for organization

--- blogfinity/backend/src/app.ts
+++ blogfinity/backend/src/app.ts
@@ -1,12 +1,10 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
-const port = 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(port, () => {
-  console.log("Server running on port: " + port);
-});
+export default app;
