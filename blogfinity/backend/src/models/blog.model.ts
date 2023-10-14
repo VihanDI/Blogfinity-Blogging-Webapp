7c1a1d4ad8f9a2e0c99a381a90a858a436b75afe
Inupa Vihan <inupavihan9@gmail.com>
2023-10-14 19:32:02 +0530

    Add blog model

---
+++ blogfinity/backend/src/models/blog.model.ts
@@ -0,0 +1,13 @@
+import { InferSchemaType, Schema } from "mongoose";
+
+const blogSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+type Blog = InferSchemaType<typeof blogSchema>;
+
+export default model<Blog>("Blog", blogSchema);
