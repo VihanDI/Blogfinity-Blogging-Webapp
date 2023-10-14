18dd069e717bfff4e0f365d8bc47643f25c268e7
Inupa Vihan <inupavihan9@gmail.com>
2023-10-14 20:12:40 +0530

    Add middleware for error handling

--- blogfinity/backend/src/app.ts
+++ blogfinity/backend/src/app.ts
@@ -1,22 +1,28 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 import BlogModel from "./models/blog.model";
 
 const app = express();
 
-app.get("/", async (req, res) => {
+app.get("/", async (req, res, next) => {
   try {
+    //throw Error("Test Error");
     const blogs = await BlogModel.find().exec();
     res.status(200).json(blogs);
   } catch (error) {
-    console.error(error);
-    let errorMessage = "An unknown error occured";
+    next(error);
+  }
+});
 
-    if (error instanceof Error) {
-      errorMessage = error.message;
-      res.status(500).json({ error: errorMessage });
-    }
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMessage = "An unknown error occured";
+
+  if (error instanceof Error) {
+    errorMessage = error.message;
+    res.status(500).json({ error: errorMessage });
   }
 });
 
