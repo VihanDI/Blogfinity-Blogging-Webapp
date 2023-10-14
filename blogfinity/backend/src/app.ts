e17973ae7792acb1ec01b4f9673fdfc17ea46996
Inupa Vihan <inupavihan9@gmail.com>
2023-10-15 00:52:39 +0530

    Add Morgan for logging
    Install Morgan
    Add middleware for logging

--- blogfinity/backend/src/app.ts
+++ blogfinity/backend/src/app.ts
@@ -1,8 +1,12 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
+import morgan from "morgan";
 import BlogRoute from "./routes/blog.route";
 
 const app = express();
+
+//middleware for logging
+app.use(morgan("dev"));
 
 //for sending json to the express server
 app.use(express.json());
