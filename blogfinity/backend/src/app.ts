f31ed83078343f813c0682a00b9f09e835a76cfd
Inupa Vihan <inupavihan9@gmail.com>
2023-10-15 01:50:38 +0530

    Refactor error handling
    Install Http-error package for error handling
    Refactor error handling methods for improved error logging

--- blogfinity/backend/src/app.ts
+++ blogfinity/backend/src/app.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
+import createHttpError, { isHttpError } from "http-errors";
 import BlogRoute from "./routes/blog.route";
 
 const app = express();
@@ -16,7 +17,7 @@ app.use("/api/blogs", BlogRoute);
 
 //middleware for handling invalid routes
 app.use((req, res, next) => {
-  next(Error("Endpoint not found"));
+  next(createHttpError(404, "Endpoint not found"));
 });
 
 //middleware for handling errors
@@ -24,11 +25,21 @@ app.use((req, res, next) => {
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
   let errorMessage = "An unknown error occured";
+  let statusCode = 500;
 
+  if (isHttpError(error)) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+
+  res.status(statusCode).json({ error: errorMessage });
+
+  /*
   if (error instanceof Error) {
     errorMessage = error.message;
-    res.status(500).json({ error: errorMessage });
+    res.status(statusCode).json({ error: errorMessage });
   }
+  */
 });
 
 export default app;
--- blogfinity/backend/src/controllers/blog.controller.ts
+++ blogfinity/backend/src/controllers/blog.controller.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express";
 import BlogModel from "../models/blog.model";
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
 
 export const getBlogs: RequestHandler = async (req, res, next) => {
   try {
@@ -15,18 +17,43 @@ export const getBlog: RequestHandler = async (req, res, next) => {
   const blogId = req.params.blogId;
 
   try {
+    //checking fot the validity of the blogId
+    if (!mongoose.isValidObjectId(blogId)) {
+      throw createHttpError(400, "Invalid blog Id");
+    }
+
     const blog = await BlogModel.findById(blogId).exec();
+
+    //handling errors of the object not found in the database
+    if (!blog) {
+      throw createHttpError(404, "Blog not found");
+    }
+
     res.status(200).json(blog);
   } catch (error) {
     next(error);
   }
 };
 
-export const createBlog: RequestHandler = async (req, res, next) => {
+interface CreateBlogBody {
+  title?: string; // marked as optional since the value can be undefine before handling the errors
+  content?: string; // if this is optional use content? instead of content
+}
+
+export const createBlog: RequestHandler<
+  unknown,
+  unknown,
+  CreateBlogBody,
+  unknown
+> = async (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
 
   try {
+    if (!title) {
+      throw createHttpError(400, "Blog must have a title");
+    }
+
     const newBlog = await BlogModel.create({
       title: title,
       content: content,
