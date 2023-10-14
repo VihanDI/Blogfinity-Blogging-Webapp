a80cbd16d0434fcdcd84a250006640c9816ee1a1
Inupa Vihan <inupavihan9@gmail.com>
2023-10-14 20:02:53 +0530

    Add error handling
    Add error handling for the test endpoint

--- blogfinity/backend/src/app.ts
+++ blogfinity/backend/src/app.ts
@@ -6,8 +6,18 @@ import BlogModel from "./models/blog.model";
 const app = express();
 
 app.get("/", async (req, res) => {
-  const blogs = await BlogModel.find().exec();
-  res.status(200).json(blogs);
+  try {
+    const blogs = await BlogModel.find().exec();
+    res.status(200).json(blogs);
+  } catch (error) {
+    console.error(error);
+    let errorMessage = "An unknown error occured";
+
+    if (error instanceof Error) {
+      errorMessage = error.message;
+      res.status(500).json({ error: errorMessage });
+    }
+  }
 });
 
 export default app;
