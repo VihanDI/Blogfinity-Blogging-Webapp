c39e4cdb75213f237afac222eb30ad761280b170
Inupa Vihan <inupavihan9@gmail.com>
2023-10-15 23:12:52 +0530

    Add fetch blogs to frontend
    Add method to fetch blogs to frontend
    Add blog model in frontend
    Install cors package
    Add middleware to setup cors for requests to the backend server

--- blogfinity/frontend/src/App.tsx
+++ blogfinity/frontend/src/App.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { BlogModel } from "./models/blog.model";
 
 function App() {
-  return <div></div>;
+  const [blogs, setBlogs] = useState<BlogModel[]>([]);
+
+  useEffect(() => {
+    async function loadBlogs() {
+      try {
+        const response = await fetch("http://localhost:8080/api/blogs", {
+          method: "GET",
+        });
+
+        const blogs = await response.json();
+        setBlogs(blogs);
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+    }
+
+    loadBlogs();
+  }, []);
+
+  return <div className="App">{JSON.stringify(blogs)}</div>;
 }
 
 export default App;
