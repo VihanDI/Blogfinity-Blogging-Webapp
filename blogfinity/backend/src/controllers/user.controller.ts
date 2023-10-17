ed444c7c71799226f7315a1c92d90976e0307739
Inupa Vihan <inupavihan9@gmail.com>
2023-10-17 21:31:58 +0530

    Add user controller
    Install bcrypt for hashing the password
    Add user controller to handle the endpoints
    Add unique property to email and username in the user model

---
+++ blogfinity/backend/src/controllers/user.controller.ts
@@ -0,0 +1,54 @@
+import { RequestHandler } from "express";
+import bcrypt from "bcrypt";
+import createHttpError from "http-errors";
+import userModel from "../models/user.model";
+
+interface SignUpBody {
+  username?: string;
+  email?: string;
+  password?: string;
+}
+
+export const signUp: RequestHandler<
+  unknown,
+  unknown,
+  SignUpBody,
+  unknown
+> = async (req, res, next) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const passwordRaw = req.body.password;
+
+  try {
+    if (!username || !email || !passwordRaw) {
+      throw createHttpError(400, "Parameters missing");
+    }
+
+    const existingUsername = await userModel
+      .findOne({ username: username })
+      .exec();
+
+    if (existingUsername) {
+      throw createHttpError(400, "Username already exists");
+    }
+
+    const existingEmail = await userModel.findOne({ email: email }).exec();
+
+    if (existingEmail) {
+      throw createHttpError(400, "Already have an account, Login");
+    }
+
+    //hashing the password
+    const passwordHashed = await bcrypt.hash(passwordRaw, 10);
+
+    const newUser = await userModel.create({
+      username: username,
+      email: email,
+      password: passwordHashed,
+    });
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+};
