22294f1cba53ee1a5f68875d9871ae12a3807c8a
Inupa Vihan <inupavihan9@gmail.com>
2023-10-19 22:57:52 +0530

    Add signup from frontend

--- blogfinity/frontend/src/pages/SignupPage.tsx
+++ blogfinity/frontend/src/pages/SignupPage.tsx
@@ -1,43 +1,80 @@
 import SubmitButton from "../components/SubmitButton";
-import UserInput from "../components/UserInput";
 import styles from "../styles/SignupPage.module.css";
 import { BiSolidUser, BiSolidLock, BiSolidEnvelope } from "react-icons/bi";
+import { SignUpCredentials } from "../utils/users.api";
+import * as UserApi from "../utils/users.api";
+import { UserModel } from "../models/user.model";
+import { useForm } from "react-hook-form";
 
-const SignupPage = () => {
+interface SignupPageProps {
+  onSignupSuccessful: (user: UserModel) => void;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const SignupPage = ({ onSignupSuccessful }: SignupPageProps) => {
+  const { register, handleSubmit, reset } = useForm<SignUpCredentials>();
+
+  async function onSubmit(input: SignUpCredentials) {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const onSignupSuccessful = (user: UserModel) => {};
+
+      const newUser = await UserApi.signUp(input);
+      onSignupSuccessful(newUser);
+      reset();
+    } catch (error) {
+      console.error(error);
+      alert(error);
+    }
+  }
+
   return (
-    <div className={styles.container}>
-      <div className={styles.formBox}>
-        <div className={styles.title}>
-          <p className={styles.titleText}>CREATE BLOGFINITY ACCOUNT</p>
-        </div>
-        <div className={styles.form}>
-          <div className={styles.inputField}>
-            <p className={styles.inputFieldText}>USER EMAIL</p>
-            <div className={styles.inputfieldIcon}>
-              <BiSolidEnvelope className={styles.icon}></BiSolidEnvelope>
-            </div>
-            <UserInput></UserInput>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className={styles.container}>
+        <div className={styles.formBox}>
+          <div className={styles.title}>
+            <p className={styles.titleText}>CREATE BLOGFINITY ACCOUNT</p>
           </div>
-          <div className={styles.inputField}>
-            <p className={styles.inputFieldText}>USERNAME</p>
-            <div className={styles.inputfieldIcon}>
-              <BiSolidUser className={styles.icon}></BiSolidUser>
+          <div className={styles.form}>
+            <div className={styles.inputField}>
+              <p className={styles.inputFieldText}>USER EMAIL</p>
+              <div className={styles.inputfieldIcon}>
+                <BiSolidEnvelope className={styles.icon}></BiSolidEnvelope>
+              </div>
+              <input
+                className={styles.userInput}
+                type="email"
+                {...register("email", { required: "Require" })}
+              ></input>
             </div>
-            <UserInput></UserInput>
-          </div>
-          <div className={styles.inputField}>
-            <p className={styles.inputFieldText}>PASSWORD</p>
-            <div className={styles.inputfieldIcon}>
-              <BiSolidLock className={styles.icon}></BiSolidLock>
+            <div className={styles.inputField}>
+              <p className={styles.inputFieldText}>USERNAME</p>
+              <div className={styles.inputfieldIcon}>
+                <BiSolidUser className={styles.icon}></BiSolidUser>
+              </div>
+              <input
+                className={styles.userInput}
+                {...register("username", { required: "Require" })}
+              ></input>
             </div>
-            <UserInput></UserInput>
+            <div className={styles.inputField}>
+              <p className={styles.inputFieldText}>PASSWORD</p>
+              <div className={styles.inputfieldIcon}>
+                <BiSolidLock className={styles.icon}></BiSolidLock>
+              </div>
+              <input
+                className={styles.userInput}
+                type="password"
+                {...register("password", { required: "Require" })}
+              ></input>
+            </div>
           </div>
-        </div>
-        <div className={styles.formButtonContent}>
-          <SubmitButton buttonText="SIGN UP"></SubmitButton>
+          <div className={styles.formButtonContent}>
+            <SubmitButton buttonText="SIGN UP"></SubmitButton>
+          </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
