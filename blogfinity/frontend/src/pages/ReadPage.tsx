41cf5557e4c75653527a0b3469cc7d5044fe3310
Inupa Vihan <inupavihan9@gmail.com>
2023-10-21 23:26:18 +0530

    Add minor UI improvements

--- blogfinity/frontend/src/pages/ReadPage.tsx
+++ blogfinity/frontend/src/pages/ReadPage.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { BiSearch } from "react-icons/bi";
 import { BlogModel } from "../models/blog.model";
 import Blog from "../components/Blog";
 import styles from "../styles/ReadPage.module.css";
@@ -29,18 +28,11 @@ const ReadPage = () => {
     <div className={styles.blogFeed}>
       <div className={styles.contentTopPart}>
         <p className={styles.contentText}>BLOGFINITY RECOMMENDATIONS</p>
-        <div className={styles.searchBar}>
-          <p className={styles.searchBarText}>SEARCH</p>
-          <input className={styles.searchBarInput}></input>
-          <button className={styles.searchBarButton}>
-            <BiSearch className={styles.searchBarIcon}></BiSearch>
-          </button>
-        </div>
       </div>
       <div className={styles.blogCardView}>
         {blogs.map((blog) => (
           <Blog
-            buttonText="READ"
+            buttonText=""
             blog={blog}
             handleClick={readBlog}
             handleCardClick={readBlog}
